c916deb4220c0413caf6b121ccd0a57151ae5040
TechnoTalksDev <technotalksyt101@gmail.com>
2021-07-03 15:29:50 -0700

    Added the recipe!

--- src/main/java/me/technotalks/crafftablesaddles/Main.java
+++ src/main/java/me/technotalks/crafftablesaddles/Main.java
@@ -1,6 +1,11 @@
 package me.technotalks.crafftablesaddles;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
@@ -8,12 +13,22 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println(ChatColor.GOLD+"Crafftable Saddles has loaded!");
+        System.out.println(ChatColor.GOLD+"Craftable Saddles has loaded!");
+        loadRecipe();
+    }
 
+    private void loadRecipe() {
+        ItemStack item = new ItemStack(Material.SADDLE);
+        ShapedRecipe recipe = new ShapedRecipe(NamespacedKey.minecraft("saddle"), item);
+        recipe.shape("LXL","LLL","LXL");
+        recipe.setIngredient('L', Material.LEATHER);
+        recipe.setIngredient('X', Material.STRING);
+        Bukkit.getServer().addRecipe(recipe);
     }
 
     @Override
     public void onDisable() {
+        System.out.println(ChatColor.GOLD+"Craftable Saddles has unloaded");
         // Plugin shutdown logic
     }
 }
